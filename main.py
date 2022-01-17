67485da2a86f6f52e2bccdd1e70fcec5d366b10d
Krithika N <76687631+krithika117@users.noreply.github.com>
2022-01-17 20:55:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from playsound import playsound
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end = "\r")
+        time.sleep(1)
+        t -= 1
+    print('Timer Completed!')
+    playsound('note.wav')
+
+t = input("Enter time in seconds = ")
+countdown(int(t))
+
+
